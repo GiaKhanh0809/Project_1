48cfcd7302c82a74f2e512ab986c4534bdc3876d
GiaKhanh0809 <legiakhanh8989@gmail.com>
2025-10-03 16:25:46 +0700

    Xử lý DOM trên một file HTML demo

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+// Lấy element bằng selector (querySelector)
+const h1 = document.querySelector(".title");
+const btn = document.getElementById("btn");
+
+// Thay text khi click
+btn.addEventListener("click", () => {
+  // innerHTML cho phép chèn HTML (dùng cẩn thận nếu dữ liệu từ user)
+  h1.innerHTML = "Đã thay đổi text của thẻ h1";
+});
